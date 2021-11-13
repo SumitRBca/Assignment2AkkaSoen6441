0173949d746b66da29f9be892dcc80e68e5106e4
Sanjay Shankar <sanjay.shankar@smallcase.com>
2021-11-13 10:44:04 +0530

    fix: make input a form to allow for submission via enter

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     $('#searchBox').on('input', function() {
         var href = '/lookup/'+$("#searchBox").val();
-        $('#go').prop('href', href.toString())
+        $('form').prop('action', href.toString())
     });
 
 });
